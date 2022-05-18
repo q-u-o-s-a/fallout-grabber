96e11325cc71673c0a7dbf49a59ee25363f6f119
Jakub Walczak <jakub.walczak@giracom.de>
2022-05-18 18:29:09 +0200

    add check for empty attributes

--- Controller.php
+++ Controller.php
@@ -6,8 +6,8 @@ namespace FalloutGrabber;
 
 class Controller
 {
-    private $view;
-    private $attributes;
+    private View $view;
+    private object $attributes;
 
     /**
      * Controller constructor.
@@ -17,46 +17,48 @@ class Controller
         $this->view = $view;
         $this->attributes = $this->getAttributes();
         $action = $this->attributes->action . "Action";
-        if ($action == 'Action') {
+        if ($action === 'Action') {
             $action = 'initAction';
         }
         $this->$action();
     }
 
-    public function getAttributes() {
+    public function getAttributes(): object {
         $request = $_GET;
         if (isset($_POST['set'])) {
-            $cardNr = (int)$_POST['cardNr'];
-            $action = (string)$_POST['action'];
-            $set = (string)$_POST['set'];
+            $cardNr = (int)($_POST['cardNr'] ?? null);
+            $action = (string)($POST['action'] ?? "");
+            $set = (string)($POST['set'] ?? "");
         } else {
-            $cardNr = (int)$request['cardNr'];
-            $action = (string)$request['action'];
-            $set = (string)$request['set'];
+            $cardNr = (int)($request['cardNr'] ?? null);
+            $action = (string)($request['action'] ?? "");
+            $set = (string)($request['set'] ?? "");
         }
         return (object)['cardNr' => $cardNr, 'action' => $action, 'set' => $set];
     }
 
-    public function loadCardAction() {
-        $cardRepository = new CardRepository();
-        $assets = $cardRepository->getAssets();
+    public function loadCardAction(): void {
+        if (isset($this->attributes->set)){
+            $cardRepository = new CardRepository();
+            $assets = $cardRepository->getAssets();
 
-        $cardAsset = new CardAsset($assets[$cardRepository->getAssetNrByAssetName($this->attributes->set)]);
+            $cardAsset = new CardAsset($assets[$cardRepository->getAssetNrByAssetName($this->attributes->set)]);
 
-        $cardService = new CardService();
-        $cardService->getCard($this->attributes->cardNr, $cardAsset,
-            $assets[$cardRepository->getAssetNrByAssetName($this->attributes->set)][4]);
-        echo 'OK';
+            $cardService = new CardService();
+            $cardService->getCard($this->attributes->cardNr, $cardAsset,
+                $assets[$cardRepository->getAssetNrByAssetName($this->attributes->set)][4]);
+            echo 'OK';
+        }
     }
 
-    public function initAction() {
+    public function initAction(): void {
         $this->view->header();
         $this->view->navBar();
         $this->view->content($this->view->showHello());
         $this->view->footer();
     }
 
-    public function overviewAction() {
+    public function overviewAction(): void {
         $cardRepository = new CardRepository();
         $this->view->content($this->view->overviewLeftNavigation(
             $cardRepository->getCardTypes(),
@@ -64,19 +66,21 @@ class Controller
             $this->view->introCardSet()));
     }
 
-    public function showCardSetAction() {
-        $cardRepository = new CardRepository();
-        $this->view->content($this->view->overviewLeftNavigation(
-            $cardRepository->getCardTypes(),
-            $this->attributes->set,
-            $this->view->showCardSet($this->attributes->set, $cardRepository->getAsset($this->attributes->set))));
+    public function showCardSetAction(): void {
+        if (isset($this->attributes->set)){
+            $cardRepository = new CardRepository();
+            $this->view->content($this->view->overviewLeftNavigation(
+                $cardRepository->getCardTypes(),
+                $this->attributes->set,
+                $this->view->showCardSet($this->attributes->set, $cardRepository->getAsset($this->attributes->set))));
+        }
     }
 
-    public function addAction() {
+    public function addAction(): void {
         $this->view->content($this->view->showPlus());
     }
 
-    public function showHalloAction() {
+    public function showHalloAction(): void {
         $this->view->content($this->view->showHello());
     }
 }
